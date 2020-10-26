7c132195e31c63f0f9b8bc2186319add02743f7f
yang <anyang47@hotmail.com>
2020-10-26 15:41:10 -0400

    wrap post order by in scope

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,9 @@
 class Post < ApplicationRecord
     validates :content, presence: true
+
+    # scope
+    scope :recent, -> { order("created_at DESC")}
+    # Ex:- scope :active, -> {where(:active => true)}
     
     
     belongs_to :user

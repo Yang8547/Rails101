77b375bbca93fe37da5fd381c634b22fc81d65b9
yang <anyang47@hotmail.com>
2020-10-26 17:20:39 -0400

    join & quit group action

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,6 +1,6 @@
 class GroupsController < ApplicationController
     # only signined user can access
-    before_action :authenticate_user! , only: [:new,:create,:edit, :update, :destroy]
+    before_action :authenticate_user! , only: [:new,:create,:edit, :update, :destroy,:join,:quit]
     # only owner can edit/update/delete the specific group
     before_action :find_group_and_check_permission, only: [:edit, :update, :destroy]
 
@@ -49,6 +49,32 @@ class GroupsController < ApplicationController
         
         @group.destroy
         redirect_to groups_path, alert: "Group Deleted"
+    end
+
+    def join
+        @group = Group.find(params[:id])
+        
+        if !current_user.is_member_of?(@group)
+        current_user.join!(@group)
+        flash[:notice] = "Joined!"
+        else
+        flash[:warning] = "You are Member"
+        end
+    
+        redirect_to group_path(@group)
+    end
+  
+    def quit
+        @group = Group.find(params[:id])
+    
+        if current_user.is_member_of?(@group)
+        current_user.quit!(@group)
+        flash[:alert] = "Leaved!"
+        else
+        flash[:warning] = "You are not a member of this group"
+        end
+    
+        redirect_to group_path(@group)
     end
 
     
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,10 @@ Rails.application.routes.draw do
 
   # nest route allow us to capture this relationship in our routing
   resources :groups do
+    member do
+      post :join
+      post :quit
+    end
     resources :posts
   end
 
